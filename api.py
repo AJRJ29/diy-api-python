4c178009410dfa970c9fff1350e11768e5931697
Joshua Danao <josh@Joshuas-MacBook-Air.local>
2020-08-05 20:54:16 -0700

    creating models

--- api.py
+++ api.py
@@ -5,26 +5,26 @@ from flask import request
 def home():
   return f"Welcome Home"
 
-@app.route('/widgets', methods=['GET', 'POST'])
+@app.route('/messeges', methods=['GET', 'POST'])
 def widgets():
   if request.method == 'POST':
     return f"Hello Widgets"
   else:
     return "this is the get route"
 
-@app.route('/widgets/:id')
+@app.route('/messeges/:id')
 def create_widgets():
   return f"Creating widgets"
 
 
-@app.route('/widgets/:id', methods=['GET', 'PUT'])
+@app.route('/messeges/:id', methods=['GET', 'PUT'])
 def widgets():
   if request.method == 'PUT':
     return f"Update Widgets"
   else:
     return "this is the get route"
 
-@app.route('/widgets/:id', methods=['GET', 'DELETE'])
+@app.route('/messeges/:id', methods=['GET', 'DELETE'])
 def widgets():
   if request.method == 'DELETE':
     return f"Delete Widgets"
--- models.py
+++ models.py
@@ -1,5 +1,40 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
   
-
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/flasql'
+app.config['FLASK_ENV'] = 'development'
+app.config['FLASK_APP'] = 'api.py'
+
+db = SQLAlchemy(app)
+class User(db.Model):
+  __tablename__ = 'users'
+  id = db.Column(db.Integer, primary_key=True)
+  email = db.Column(db.String, unique=True, nullable=False)
+  name = db.Column(db.String, nullable=False)
+
+  messeges = db.relationship('Post', lazy=True, back_populates='user')
+
+  def as_dict(self):
+    return {
+      "id": self.id,
+      "name": self.name,
+      "email": self.email,
+    }
+
+  def __repr__(self):
+    return f'User(id={self.id}, email="{self.email}", name="{self.name}")'
+
+class Messege(db.Model):
+  __tablename__ = 'messeges'
+  id = db.Column(db.Integer, primary_key=True)
+  content = db.Column(db.String(150), unique=True, nullable=False)
+  user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='SET NULL'))
+
+  user = db.relationship('User', lazy=True, back_populates='messeges')
+
+  def __repr__(self):
+    return f'User(id={self.id}, email="{self.email}", display_name="{self.display_name}")'
